d6a730cd3b5681adc95981f6589f3ecad04a206f
Xkulton <felix.preissner@web.de>
2023-08-12 15:38:35 +0200

    added campus III portal

---
+++ ErstigolfUnity/Assets/Scripts/Portal.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Mesh previewMesh;
+
+    private void OnTriggerEnter(Collider other) {
+        PlayerCar pc = other.GetComponentInParent<PlayerCar>();
+        if (pc){
+            pc.transform.position = target.position;
+            pc.transform.rotation = target.rotation;
+        }
+    }
+
+    private void OnDrawGizmos() {
+        if (target && previewMesh) {
+            Gizmos.color = Color.red;
+            Gizmos.DrawMesh(previewMesh, target.position, target.rotation);
+        }
+    }
+}
