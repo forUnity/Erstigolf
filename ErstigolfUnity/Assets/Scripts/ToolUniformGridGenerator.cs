e8009bc71f0e2e8c5bfc49f0645ec58eeaa7a117
Xkulton <119746058+Xkulton@users.noreply.github.com>
2023-09-26 11:04:48 +0200

    added grid gen gizmos

--- ErstigolfUnity/Assets/Scripts/ToolUniformGridGenerator.cs
+++ ErstigolfUnity/Assets/Scripts/ToolUniformGridGenerator.cs
@@ -39,4 +39,28 @@ public class ToolUniformGridGenerator : MonoBehaviour
     }
 
     private bool Intersects(Vector3 pos) => Physics.CheckSphere(pos, checkSphereRad, checkLayerMask, QueryTriggerInteraction.Ignore);
+
+    private void OnDrawGizmos() {
+        float xMin = gridMin.position.x;
+        float xMax = gridMax.position.x;
+        float zMin = gridMin.position.z;
+        float zMax = gridMax.position.z;
+        float y = gridMin.position.y + yPositionOffset;
+        Vector3 v1 = new Vector3(xMin, y, zMin);
+        Vector3 v2 = new Vector3(xMin, y, zMax);
+        Vector3 v3 = new Vector3(xMax, y, zMax);
+        Vector3 v4 = new Vector3(xMax, y, zMin);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(v1, v2);
+        Gizmos.DrawLine(v3, v2);
+        Gizmos.DrawLine(v3, v4);
+        Gizmos.DrawLine(v1, v4);
+        Gizmos.DrawLine(v1, v3);
+        Gizmos.DrawLine(v2, v4);
+
+        for (int i = 0; i < transform.childCount; i++){
+            Gizmos.DrawSphere(transform.GetChild(i).position, 0.1f);
+        }
+    }
 }
